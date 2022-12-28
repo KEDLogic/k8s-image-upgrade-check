cefd4efb93d6cd8d3e835ef37b73173669270a5b
Kyle Dozier <kyle.dozier@cfpb.gov>
2022-12-28 02:42:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 
+
+##
+
+## CREDITS
+Many thanks to al4 for their work on docker-registry-list, which was incorporated into this repo.
---
+++ src/image_upgrade_check.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import json
+import argparse
+import subprocess
+import re
+
+#import docker_registry_list
+
+import pdb
+
+def get_all_images(kube_context):
+    cmd = subprocess.run(["kubectl", f'--context={kube_context}',
+                         "get", "pods", "--all-namespaces",
+                         "--output=jsonpath='{.items[*].status.containerStatuses[*].image}'"],
+                         capture_output=True, text=True, check=True)
+
+    images = [i.replace("'", "") for i in cmd.stdout.split(' ')]
+    return images
+
+def build_image_dicts(images):
+    image_dicts = []
+
+    for image in images:
+        image_name = image.split(':')[0]
+        tag = image.split(':')[1]
+
+        if match := re.search("^.*\.[a-z]*$", image_name.split('/')[0]):
+            registry = match[0]
+            image_name = '/'.join(image_name.split('/')[1:])
+        else:
+            registry = 'docker.io'
+
+        image_dict = {
+            "registry": registry,
+            "image_name": image_name,
+            "tag": tag
+        }
+        
+        image_dicts.append(image_dict)
+
+    return image_dicts
+
+def get_current_context():
+    if cmd :=subprocess.run(["kubectl", "config", "current-context"], capture_output=True, text=True, check=True):
+        return cmd.stdout.strip()
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+
+    p.add_argument('-k', '--kube-context',
+                   help='Context for kubectl. Defaults to current-context if not set')
+
+    args = p.parse_args()
+
+    kube_context = args.kube_context or get_current_context()
+    
+    images = build_image_dicts(get_all_images(kube_context))
+
+    print(json.dumps(images))
